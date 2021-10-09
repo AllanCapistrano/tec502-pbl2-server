5a68683600cffc7b405027e795402dc6ea2bf696
AllanCapistrano <allan.capistrano3@gmail.com>
2021-10-09 11:26:26 -0300

    Remove temporary functionality and add removeAllPatientsDevices method.

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,22 +1,16 @@
 package main;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.BindException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import models.PatientDevice;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import utils.ComparePatients;
 
 /**
  * Classe do Server.
@@ -28,7 +22,6 @@ public class Server {
     /*-------------------------- Constantes ----------------------------------*/
     private static final String IP_ADDRESS = "localhost";
     private static final int PORT = 12244;
-    private static final int DEFAULT_AMOUNT_PATIENTS = 5;
     /*------------------------------------------------------------------------*/
 
     private static ServerSocket server;
@@ -43,72 +36,6 @@ public class Server {
 
         System.out.println("> Iniciando o servidor");
         System.out.println("> Aguardando conexão");
-
-        /* -- TEMPORÁRIO -- */
-        Socket connFog = new Socket("localhost", 12245);
-
-        JSONObject json = new JSONObject();
-
-        /* Definindo os dados que serão enviadas para o Fog Server. */
-        json.put("method", "GET"); // Método HTTP
-        json.put("route", "/patients/" + DEFAULT_AMOUNT_PATIENTS); // Rota
-
-        try {
-            ObjectOutputStream output
-                    = new ObjectOutputStream(connFog.getOutputStream());
-            
-            /* Enviando a requisição para o Fog Server. */
-            output.flush();
-            output.writeObject(json);
-
-            /* Recebendo a resposta do Fog Server. */
-            ObjectInputStream input 
-                    = new ObjectInputStream(connFog.getInputStream());
-            
-            JSONObject jsonResponse = (JSONObject) input.readObject();
-            
-            JSONArray jsonArray = jsonResponse.getJSONArray("body");
-
-            /* Adicionando os dispositivos dos pacientes em uma lista.*/
-            for (int i = 0; i < jsonArray.length(); i++) {
-                patientDevices.add(
-                    new PatientDevice(
-                        jsonArray.getJSONObject(i).
-                                getString("name"),
-                        jsonArray.getJSONObject(i).
-                                getString("deviceId"),
-                        jsonArray.getJSONObject(i).
-                                getFloat("bodyTemperature"),
-                        jsonArray.getJSONObject(i).
-                                getInt("respiratoryFrequency"),
-                        jsonArray.getJSONObject(i).
-                                getFloat("bloodOxygenation"),
-                        jsonArray.getJSONObject(i).
-                                getInt("bloodPressure"),
-                        jsonArray.getJSONObject(i).
-                                getInt("heartRate"),
-                        jsonArray.getJSONObject(i).
-                                getBoolean("isSeriousCondition"),
-                        jsonArray.getJSONObject(i).
-                                getString("isSeriousConditionLabel"),
-                        jsonArray.getJSONObject(i).
-                                getFloat("patientSeverityLevel")        
-                    )
-                );
-            }
-
-            Collections.sort(patientDevices, new ComparePatients());
-           
-            output.close();            
-            input.close();
-        } catch (IOException ioe) {
-            System.err.println("Erro ao requisitar uma certa quantidade de "
-                    + "pacientes para a Fog.");
-            System.out.println(ioe);
-        }
-
-        connFog.close();
-        /* ------------ */
 
         try {
             /* Definindo o endereço e a porta do servidor. */
@@ -188,20 +115,27 @@ public class Server {
                 .findFirst()
                 .orElse(null) != null);
     }
-    
+
     /**
      * Retorna o dispositivo do paciente conforme o identificador.
-     * 
+     *
      * @param deviceId String - Identificador do dispositivo.
      * @return PatientDevice
      */
-    public static PatientDevice getPatientDeviceById(String deviceId){
+    public static PatientDevice getPatientDeviceById(String deviceId) {
         for (int i = 0; i < patientDevices.size(); i++) {
-            if(patientDevices.get(i).getDeviceId().equals(deviceId)){
+            if (patientDevices.get(i).getDeviceId().equals(deviceId)) {
                 return patientDevices.get(i);
             }
         }
-        
+
         return null;
+    }
+
+    /**
+     * Remove todos os elementos da lista de dispositivos dos pacientes.
+     */
+    public static void removeAllPatientsDevices() {
+        patientDevices.removeAll(patientDevices);
     }
 }
