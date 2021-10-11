a32f7471827235f1e9bad6d82d5a258f25114f6e
JoaoErick <jerick1415@outlook.com>
2021-10-11 18:24:12 -0300

    Add FogServer.

---
+++ src/models/FogServer.java
@@ -0,0 +1,26 @@
+
+package models;
+
+/**
+ * Classe responsável por se comunicar com o servidor socket via TCP.
+ * 
+ * @author Allan Capistrano e João Erick Barbosa
+ */
+public class FogServer {
+    private final String address;
+    private final int port;
+    
+    public FogServer(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+    
+}
