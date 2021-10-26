af2d77cec3c57ffab27abdcae1d0300229edca7d
JoaoErick <jerick1415@outlook.com>
2021-10-26 17:51:55 -0300

    Remove constructor.

--- src/models/PatientDevice.java
+++ src/models/PatientDevice.java
@@ -25,51 +25,6 @@ public class PatientDevice implements Serializable {
      * Método construtor.
      *
      * @param name String - Nome do paciente.
-     * @param bodyTemperature float - Valor da temperatura corporal registrada
-     * pelo sensor.
-     * @param respiratoryFrequency int - Valor da frequência respiratória
-     * registrada pelo sensor.
-     * @param bloodOxygenation float - Nível de oxigênio no sangue registrado
-     * pelo sensor.
-     * @param bloodPressure int - Pressão arterial registrada pelo sensor.
-     * @param heartRate int - Frequência cardíaca registrada pelo sensor.
-     * @param deviceId String - Identificador do dispositivo do paciente.
-     * @param isSeriousCondition boolean - Condição atual do paciente.
-     * @param isSeriousConditionLabel String - Condição atual do paciente em
-     * formato de texto.
-     * @param patientSeverityLevel float - Nível de gravidade do paciente.
-     * @param fogServer FogServer - Fog a qual o dispositivo está conectado.
-     */
-    public PatientDevice(
-            String name,
-            String deviceId,
-            float bodyTemperature,
-            int respiratoryFrequency,
-            float bloodOxygenation,
-            int bloodPressure,
-            int heartRate,
-            boolean isSeriousCondition,
-            String isSeriousConditionLabel,
-            float patientSeverityLevel,
-            FogServer fogServer
-    ) {
-        this.name = name;
-        this.deviceId = deviceId;
-        this.bodyTemperature = bodyTemperature;
-        this.respiratoryFrequency = respiratoryFrequency;
-        this.bloodOxygenation = bloodOxygenation;
-        this.bloodPressure = bloodPressure;
-        this.heartRate = heartRate;
-        this.isSeriousCondition = isSeriousCondition;
-        this.isSeriousConditionLabel = isSeriousConditionLabel;
-        this.patientSeverityLevel = patientSeverityLevel;
-        this.fogServer = fogServer;
-    }
-
-    /**
-     * Método construtor.
-     *
-     * @param name String - Nome do paciente.
      * @param deviceId String - Identificador do dispositivo do paciente.
      * @param isSeriousConditionLabel String - Condição atual do paciente em
      * formato de texto.
