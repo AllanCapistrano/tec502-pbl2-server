d6b3e921bdea595cc2f5e7ef002e8da147272ced
AllanCapistrano <allan.capistrano3@gmail.com>
2021-10-26 15:09:04 -0300

    Implements Serializable.

--- src/models/FogServer.java
+++ src/models/FogServer.java
@@ -1,15 +1,17 @@
-
 package models;
+
+import java.io.Serializable;
 
 /**
  * Classe responsável por se comunicar com o servidor socket via TCP.
- * 
+ *
  * @author Allan Capistrano e João Erick Barbosa
  */
-public class FogServer {
+public class FogServer implements Serializable {
+
     private final String address;
     private final int port;
-    
+
     public FogServer(String address, int port) {
         this.address = address;
         this.port = port;
@@ -22,5 +24,5 @@ public class FogServer {
     public int getPort() {
         return port;
     }
-    
+
 }
